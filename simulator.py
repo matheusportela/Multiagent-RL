67c4d875163c18c5a6afe1d4fde115b7868b2d26
Matheus Portela <matheus.v.portela@gmail.com>
2015-08-17 10:06:28 -0300

    Refactoring simulator initialization script.

--- simulator.py
+++ simulator.py
@@ -1,22 +1,40 @@
 from simulator import pacman as pacman_simulator
-from simulator import layout
+from simulator import layout as simulator_layout
 from simulator import textDisplay
 from simulator import graphicsDisplay
 from simulator import myAgents
 
-if __name__ == '__main__':
-    layout_file = 'mediumClassic'
-    layout = layout.getLayout(layout_file)
-    if layout == None: raise Exception("The layout " + layout_file + " cannot be found")
-    pacman = myAgents.RandomPacmanAgent()
-    num_ghosts = 4
-    ghosts = [myAgents.RandomGhostAgent(i+1) for i in range(num_ghosts)]
-    is_text_only = False
-    if is_text_only:
+def create_layout(layout_file):
+    layout = simulator_layout.getLayout(layout_file)
+
+    if layout == None:
+        raise Exception("The layout " + layout_file + " cannot be found")
+
+    return layout
+
+def create_pacman():
+    return myAgents.RandomPacmanAgent()
+
+def create_ghosts(num_ghosts):
+    return [myAgents.RandomGhostAgent(i+1) for i in range(num_ghosts)]
+
+def create_display(text_only=False, zoom=1.0, frameTime=0.1):
+    if text_only:
         display = textDisplay.PacmanGraphics()
     else:
-        zoom = 1.0
-        display = graphicsDisplay.PacmanGraphics(zoom, frameTime=0.1)
-    numGames = 1
+        display = graphicsDisplay.PacmanGraphics(zoom, frameTime=frameTime)
+
+    return display
+
+if __name__ == '__main__':
+    layout_file = 'mediumClassic'
+    num_ghosts = 4
+    num_games = 1
     record = False
-    pacman_simulator.runGames(layout, pacman, ghosts, display, numGames, record)
+
+    layout = create_layout(layout_file)
+    pacman = create_pacman()
+    ghosts = create_ghosts(num_ghosts)
+    display = create_display(text_only=False)
+
+    pacman_simulator.runGames(layout, pacman, ghosts, display, num_games, record)
