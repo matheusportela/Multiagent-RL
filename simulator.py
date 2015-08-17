ffdb4286ec19ccff7b7c3d38cfb99108f762beb9
Matheus Portela <matheus.v.portela@gmail.com>
2015-08-17 09:59:36 -0300

    Allowing both simulator graphical and text display.

--- simulator.py
+++ simulator.py
@@ -1,6 +1,6 @@
 from simulator import pacman as pacman_simulator
 from simulator import layout
-# from simulator import textDisplay
+from simulator import textDisplay
 from simulator import graphicsDisplay
 from simulator import myAgents
 
@@ -11,9 +11,12 @@ if __name__ == '__main__':
     pacman = myAgents.RandomPacmanAgent()
     num_ghosts = 4
     ghosts = [myAgents.RandomGhostAgent(i+1) for i in range(num_ghosts)]
-    # display = textDisplay.PacmanGraphics()
-    zoom = 1.0
-    display = graphicsDisplay.PacmanGraphics(zoom, frameTime=0.1)
+    is_text_only = False
+    if is_text_only:
+        display = textDisplay.PacmanGraphics()
+    else:
+        zoom = 1.0
+        display = graphicsDisplay.PacmanGraphics(zoom, frameTime=0.1)
     numGames = 1
     record = True
     pacman_simulator.runGames(layout, pacman, ghosts, display, numGames, record)
