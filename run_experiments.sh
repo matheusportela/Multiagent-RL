2ed2e82b3b2035abbc8350d85650d2266e498767
Matheus Portela <matheus.v.portela@gmail.com>
2015-11-25 07:26:24 -0200

    Repeating experiments to collect data.

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,26 +2,48 @@
 OUTPUT_PATH=experiments
 LEARNING_GAMES=100
 TESTING_GAMES=15
+NUM_REPETITIONS=10
+
+OUTPUT_1=$OUTPUT_PATH/noiseless/ghost_ai
+OUTPUT_2=$OUTPUT_PATH/noise/ghost_ai
+OUTPUT_3=$OUTPUT_PATH/noiseless/ghost_ai_pacman_eater
+OUTPUT_4=$OUTPUT_PATH/noise/ghost_ai_pacman_eater
 
 rm -rf $OUTPUT_PATH
 mkdir $OUTPUT_PATH
-mkdir $OUTPUT_PATH/ghost_ai
-mkdir $OUTPUT_PATH/ghost_ai_noise
-mkdir $OUTPUT_PATH/ghost_ai_pacman_eater
-mkdir $OUTPUT_PATH/ghost_ai_pacman_eater_noise
+mkdir -p $OUTPUT_1
+mkdir -p $OUTPUT_2
+mkdir -p $OUTPUT_3
+mkdir -p $OUTPUT_4
 
-#python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 2 -o $OUTPUT_PATH/ghost_ai/results2.txt -p $OUTPUT_PATH/ghost_ai/policies2.txt
-#python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 3 -o $OUTPUT_PATH/ghost_ai/results3.txt -p $OUTPUT_PATH/ghost_ai/policies3.txt
-#python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 4 -o $OUTPUT_PATH/ghost_ai/results4.txt -p $OUTPUT_PATH/ghost_ai/policies4.txt
+# AI Ghost vs. Random Pacman - Noiseless
+for i in `seq $NUM_REPETITIONS`
+do
+  #python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 2 -o $OUTPUT_1/results2_$i.txt -p $OUTPUT_1/policies2_$i.txt
+  #python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 3 -o $OUTPUT_1/results3_$i.txt -p $OUTPUT_1/policies3_$i.txt
+  #python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 4 -o $OUTPUT_1/results4_$i.txt -p $OUTPUT_1/policies4_$i.txt
+done
 
-#python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 2 -o $OUTPUT_PATH/ghost_ai_noise/results2.txt -p $OUTPUT_PATH/ghost_ai_noise/policies2.txt --noise 3
-#python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 3 -o $OUTPUT_PATH/ghost_ai_noise/results3.txt -p $OUTPUT_PATH/ghost_ai_noise/policies3.txt --noise 3
-#python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 4 -o $OUTPUT_PATH/ghost_ai_noise/results4.txt -p $OUTPUT_PATH/ghost_ai_noise/policies4.txt --noise 3
+# AI Ghost vs. Random Pacman - Noise
+for i in `seq $NUM_REPETITIONS`
+do
+  #python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 2 -o $OUTPUT_2/results2_$i.txt -p $OUTPUT_2/policies2_$i.txt --noise 3
+  #python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 3 -o $OUTPUT_2/results3_$i.txt -p $OUTPUT_2/policies3_$i.txt --noise 3
+  #python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent random --ghost-agent ai -e 4 -o $OUTPUT_2/results4_$i.txt -p $OUTPUT_2/policies4_$i.txt --noise 3
+done
 
-python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 2 -o $OUTPUT_PATH/ghost_ai_pacman_eater/results2.txt -p $OUTPUT_PATH/ghost_ai_pacman_eater/policies2.txt
-# python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 3 -o $OUTPUT_PATH/ghost_ai_pacman_eater/results3.txt -p $OUTPUT_PATH/ghost_ai_pacman_eater/policies3.txt
-# python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 4 -o $OUTPUT_PATH/ghost_ai_pacman_eater/results4.txt -p $OUTPUT_PATH/ghost_ai_pacman_eater/policies4.txt
+# AI Ghost vs. Greedy Pacman - Noiseless
+for i in `seq $NUM_REPETITIONS`
+do
+  python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 2 -o $OUTPUT_3/results2_$i.txt -p $OUTPUT_3/policies2_$i.txt
+  # python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 3 -o $OUTPUT_3/results3_$i.txt -p $OUTPUT_3/policies3_$i.txt
+  # python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 4 -o $OUTPUT_3/results4_$i.txt -p $OUTPUT_3/policies4_$i.txt
+done
 
-python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 2 -o $OUTPUT_PATH/ghost_ai_pacman_eater_noise/results2.txt -p $OUTPUT_PATH/ghost_ai_pacman_eater_noise/policies2.txt --noise 3
-# python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 3 -o $OUTPUT_PATH/ghost_ai_pacman_eater_noise/results3.txt -p $OUTPUT_PATH/ghost_ai_pacman_eater_noise/policies3.txt --noise 3
-# python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 4 -o $OUTPUT_PATH/ghost_ai_pacman_eater_noise/results4.txt -p $OUTPUT_PATH/ghost_ai_pacman_eater_noise/policies4.txt --noise 3
+# AI Ghost vs. Greedy Pacman - Noise
+for i in `seq $NUM_REPETITIONS`
+do
+  python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 2 -o $OUTPUT_4/results2_$i.txt -p $OUTPUT_PATH_4/policies2_$i.txt --noise 3
+  # python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 3 -o $OUTPUT_4/results3_$i.txt -p $OUTPUT_4/policies3_$i.txt --noise 3
+  # python simulator.py -l $LEARNING_GAMES -t $TESTING_GAMES --pacman-agent eater --ghost-agent ai -e 4 -o $OUTPUT_4/results4_$i.txt -p $OUTPUT_4/policies4_$i.txt --noise 3
+done
