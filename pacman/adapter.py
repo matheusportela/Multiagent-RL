745ed396c05fb62294a7a7e4fc1e8a2e574f88ae
Pedro Saman <pedrosaman96@gmail.com>
2016-06-07 15:57:09 -0300

    Fixed an error on graphics
    The graphics module had some problem so I did this change and it start working.

--- pacman/adapter.py
+++ pacman/adapter.py
@@ -115,7 +115,7 @@ class Adapter(object):
         self.output_file = str(output_file)
 
         if graphics:
-            self.display = BerkeleyPacmanGraphics(zoom, frameTime=frameTime)
+            self.display = BerkeleyPacmanGraphics()
         else:
             self.display = BerkeleyNullGraphics()
 
