6eb3581ef0af34f9a1e942e35d24d30fddd7a62e
Renato Nobre <rekanobre@gmail.com>
2016-08-24 15:05:26 -0300

    Behaviors Fixing Part 1

--- pacman/behaviors.py
+++ pacman/behaviors.py
@@ -2,12 +2,13 @@
 
 import random
 
+
 class Behavior(object):
     def __str__(self):
         return self.__class__.__name__
 
     def __call__(self, state, legal_actions):
-        raise NotImplementedError, 'Behavior must implement __call__'
+        raise NotImplementedError('Behavior must implement __call__')
 
 
 class RandomBehavior(Behavior):
@@ -31,13 +32,14 @@ class EatBehavior(Behavior):
 
         for action in legal_actions:
             diff = agent_map.action_to_pos[action]
-            new_position = (agent_position[0] + diff[0], agent_position[1] + diff[1])
+            new_position = (agent_position[0] + diff[0],
+                            agent_position[1] + diff[1])
 
             for x in range(food_map.width):
                 for y in range(food_map.height):
                     new_distance = state.calculate_distance(new_position, (y, x))
 
-                    if (best_action == None) or (food_map[y][x] > food_prob_threshold and new_distance < min_dist):
+                    if (best_action is None) or (food_map[y][x] > food_prob_threshold and new_distance < min_dist):
                         min_dist = new_distance
                         best_action = action
 
@@ -57,10 +59,11 @@ class FleeBehavior(Behavior):
 
         for action in legal_actions:
             diff = agent_map.action_to_pos[action]
-            new_position = (agent_position[0] + diff[0], agent_position[1] + diff[1])
+            new_position = (agent_position[0] + diff[0],
+                            agent_position[1] + diff[1])
             new_distance = state.calculate_distance(new_position, enemy_position)
 
-            if (best_action == None) or (agent_map._is_valid_position(new_position) and
+            if (best_action is None) or (agent_map._is_valid_position(new_position) and
                 new_distance > max_distance):
                 best_action = action
                 max_distance = new_distance
@@ -81,10 +84,11 @@ class SeekBehavior(Behavior):
 
         for action in legal_actions:
             diff = agent_map.action_to_pos[action]
-            new_position = (agent_position[0] + diff[0], agent_position[1] + diff[1])
+            new_position = (agent_position[0] + diff[0],
+                            agent_position[1] + diff[1])
             new_distance = state.calculate_distance(new_position, enemy_position)
 
-            if (best_action == None) or (agent_map._is_valid_position(new_position) and
+            if (best_action is None) or (agent_map._is_valid_position(new_position) and
                 new_distance < min_distance):
                 best_action = action
                 min_distance = new_distance
