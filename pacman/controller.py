6bea5a3b9bbc24eb01bd2d2b46efecff4cbdbb02
Matheus Portela <matheus.v.portela@gmail.com>
2016-10-04 20:28:07 -0300

    Refactoring base controller class

--- pacman/controller.py
+++ pacman/controller.py
@@ -17,6 +17,7 @@ from state import GameState
 # @todo properly include communication module from parent folder
 import sys
 sys.path.insert(0, '..')
+import core
 import communication
 
 __author__ = "Matheus Portela and Guilherme N. Ramos"
@@ -30,10 +31,9 @@ def log(msg):
     print '[Controller] {}'.format(msg)
 
 
-class Controller(communication.ZMQServer):
-    """Keeps the agent states and controls messages to clients."""
-    def __init__(self, context, binding):
-        super(Controller, self).__init__(context, binding)
+class PacmanController(core.BaseController):
+    def __init__(self, *args, **kwargs):
+        super(PacmanController, self).__init__(*args, **kwargs)
 
         self.agents = {}
         self.agent_classes = {}
@@ -41,37 +41,43 @@ class Controller(communication.ZMQServer):
         self.game_states = {}
         self.game_number = {}
 
-        log('Ready!')
+    def start(self):
+        log('Starting')
 
-    def _choose_action(self, state):
-        """Update agent state."""
-        for id_, pos in state.agent_positions.items():
-            self.game_states[state.agent_id].observe_agent(id_, pos)
+    def stop(self):
+        log('Stopped')
 
-        for id_, status in state.fragile_agents.items():
-            self.game_states[state.agent_id].observe_fragile_agent(id_, status)
+    def step(self):
+        pass
 
-        """Choose action"""
-        agent_state = self.game_states[state.agent_id]
-        choose_action = self.agents[state.agent_id].choose_action
-        agent_action = choose_action(agent_state, state.executed_action,
-                                     state.reward, state.legal_actions,
-                                     state.test_mode)
+    def is_finished(self):
+        return False
 
-        for id_ in self.game_states:
-            agent_state.predict_agent(id_, agent_action)
+    def communicate(self):
+        message = self.receive()
+        reply_message = self._reply(message)
+        self.send(reply_message)
 
-        return agent_action
+    def _reply(self, msg):
+        reply_msg = None
 
-    def _get_allies(self, agent_id):
-        return [id_ for id_ in self.agent_teams
-                if self.agent_teams[id_] == self.agent_teams[agent_id] and
-                id_ != agent_id]
+        if msg.type == messages.REQUEST_INIT_MSG:
+            reply_msg = self._initialize_agent(msg)
+        elif msg.type == messages.REQUEST_GAME_START_MSG:
+            reply_msg = self._start_game_for_agent(msg)
+            self.game_number[msg.agent_id] += 1
+        elif msg.type == messages.REQUEST_REGISTER_MSG:
+            reply_msg = self._register_agent(msg)
+        elif msg.type == messages.REQUEST_BEHAVIOR_COUNT_MSG:
+            reply_msg = self._request_behavior_count(msg.agent_id)
+        elif msg.type == messages.REQUEST_POLICY_MSG:
+            reply_msg = self._send_policy_request(msg)
+        elif msg.type == messages.STATE_MSG:
+            reply_msg = self._send_agent_action(msg)
+        elif msg.type == messages.POLICY_MSG:
+            reply_msg = self._set_agent_policy(msg)
 
-    def _get_enemies(self, agent_id):
-        return [id_ for id_ in self.agent_teams
-                if self.agent_teams[id_] != self.agent_teams[agent_id] and
-                id_ != agent_id]
+        return reply_msg
 
     def _initialize_agent(self, msg):
         agent_id = msg.agent_id
@@ -88,62 +94,17 @@ class Controller(communication.ZMQServer):
         log('Initialized {} #{}'.format(self.agent_teams[agent_id], agent_id))
 
         reply_msg = messages.AckMessage()
-        self.send(reply_msg)
+        return reply_msg
 
-    def _register_agent(self, msg):
-        self.agent_classes[msg.agent_id] = msg.agent_class
-        self.agent_teams[msg.agent_id] = msg.agent_team
+    def _get_allies(self, agent_id):
+        return [id_ for id_ in self.agent_teams
+                if self.agent_teams[id_] == self.agent_teams[agent_id] and
+                id_ != agent_id]
 
-        log('Registered {} #{}'.format(msg.agent_class.__name__, msg.agent_id))
-
-        reply_msg = messages.AckMessage()
-        self.send(reply_msg)
-
-    def _reply(self, msg):
-        if msg.type == messages.STATE_MSG:
-            self.last_action = self._send_agent_action(msg)
-        elif msg.type == messages.REQUEST_INIT_MSG:
-            self._initialize_agent(msg)
-        elif msg.type == messages.REQUEST_GAME_START_MSG:
-            self._start_game_for_agent(msg)
-            self.game_number[msg.agent_id] += 1
-        elif msg.type == messages.REQUEST_REGISTER_MSG:
-            self._register_agent(msg)
-        elif msg.type == messages.REQUEST_BEHAVIOR_COUNT_MSG:
-            self._request_behavior_count(msg.agent_id)
-        elif msg.type == messages.REQUEST_POLICY_MSG:
-            self._send_policy_request(msg)
-        elif msg.type == messages.POLICY_MSG:
-            self._set_agent_policy(msg)
-
-    def _request_behavior_count(self, agent_id):
-        count = self.agents[agent_id].behavior_count
-        reply_msg = messages.BehaviorCountMessage(count)
-        self.send(reply_msg)
-
-        self.agents[agent_id].reset_behavior_count()
-
-    def _send_agent_action(self, msg):
-        game_state = self.game_states[msg.agent_id]
-        # @todo is it necessary to set walls every time?
-        game_state.set_walls(msg.wall_positions)
-        game_state.set_food_positions(msg.food_positions)
-
-        agent_action = self._choose_action(msg)
-        reply_msg = messages.ActionMessage(msg.agent_id, agent_action)
-        self.send(reply_msg)
-
-        return agent_action
-
-    def _send_policy_request(self, msg):
-        policy = self.agents[msg.agent_id].get_policy()
-        reply_message = messages.PolicyMessage(msg.agent_id, policy)
-        self.send(reply_message)
-
-    def _set_agent_policy(self, msg):
-        if msg.policy:
-            self.agents[msg.agent_id].set_policy(msg.policy)
-        self.send(messages.AckMessage())
+    def _get_enemies(self, agent_id):
+        return [id_ for id_ in self.agent_teams
+                if self.agent_teams[id_] != self.agent_teams[agent_id] and
+                id_ != agent_id]
 
     def _start_game_for_agent(self, msg):
         ally_ids = self._get_allies(msg.agent_id)
@@ -165,31 +126,75 @@ class Controller(communication.ZMQServer):
                                                    iteration=iteration)
 
         reply_msg = messages.AckMessage()
-        self.send(reply_msg)
         log('Start game for {} #{}'.format(self.agent_teams[msg.agent_id],
                                            msg.agent_id))
+        return reply_msg
 
-    def run(self):
-        log('Now running')
+    def _register_agent(self, msg):
+        self.agent_classes[msg.agent_id] = msg.agent_class
+        self.agent_teams[msg.agent_id] = msg.agent_team
 
-        self.last_action = agents.FIRST_ACTION
+        log('Registered {} #{}'.format(msg.agent_class.__name__, msg.agent_id))
 
-        while True:
-            msg = self.receive()
-            self._reply(msg)
+        reply_msg = messages.AckMessage()
+        return reply_msg
+
+    def _request_behavior_count(self, agent_id):
+        count = self.agents[agent_id].behavior_count
+        reply_msg = messages.BehaviorCountMessage(count)
+        self.agents[agent_id].reset_behavior_count()
+        return reply_msg
+
+    def _send_policy_request(self, msg):
+        policy = self.agents[msg.agent_id].get_policy()
+        reply_message = messages.PolicyMessage(msg.agent_id, policy)
+        return reply_message
+
+    def _send_agent_action(self, msg):
+        game_state = self.game_states[msg.agent_id]
+        # @todo is it necessary to set walls every time?
+        game_state.set_walls(msg.wall_positions)
+        game_state.set_food_positions(msg.food_positions)
+
+        agent_action = self._choose_action(msg)
+        return messages.ActionMessage(msg.agent_id, agent_action)
+
+    def _choose_action(self, state):
+        # Update agent state
+        for id_, pos in state.agent_positions.items():
+            self.game_states[state.agent_id].observe_agent(id_, pos)
+
+        for id_, status in state.fragile_agents.items():
+            self.game_states[state.agent_id].observe_fragile_agent(id_, status)
+
+        # Choose action
+        agent_state = self.game_states[state.agent_id]
+        choose_action = self.agents[state.agent_id].choose_action
+        agent_action = choose_action(agent_state, state.executed_action,
+                                     state.reward, state.legal_actions,
+                                     state.test_mode)
+
+        for id_ in self.game_states:
+            agent_state.predict_agent(id_, agent_action)
+
+        return agent_action
+
+    def _set_agent_policy(self, msg):
+        if msg.policy:
+            self.agents[msg.agent_id].set_policy(msg.policy)
+        return messages.AckMessage()
 
 
 def build_controller(context=None, endpoint=None,
                      port=communication.DEFAULT_TCP_PORT):
     if context and endpoint:
-        binding = 'inproc://{}'.format(endpoint)
+        server = communication.InprocServer(context, endpoint)
         log('Connecting with inproc communication')
     else:
-        context = zmq.Context()
-        binding = 'tcp://*:{}'.format(port)
+        server = communication.TCPServer(port)
         log('Connecting with TCP communication (port {})'.format(port))
 
-    return Controller(context, binding)
+    return PacmanController(server=server)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
